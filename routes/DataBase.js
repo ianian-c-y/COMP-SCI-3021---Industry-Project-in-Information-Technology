2e4dd519b1401679eb3c49191ac0b9da6fc2dae9
ianian-c-y <161934550+ianian-c-y@users.noreply.github.com>
2025-08-24 18:49:51 +0930

    /

--- routes/DataBase.js
+++ routes/DataBase.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const cvePool = mysql.createPool({
     host: '127.0.0.1',
     port: '3306',
-    user: 'Userss',
+    user: 'User',
     password: 'PassWord',
     database: 'CVE_DataBase',
     waitForConnections: true,
